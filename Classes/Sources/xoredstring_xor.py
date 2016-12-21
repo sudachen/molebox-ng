73525eda80b6b7ad03b7c4d06766942a8c69663a
Alexey Sudachen <alexey@sudachen.name>
2016-12-20 22:20:17 -0300

    updated

--- Classes/Sources/xoredstring_xor.py
+++ Classes/Sources/xoredstring_xor.py
@@ -8,18 +8,18 @@
 import sys
 
 name = sys.argv[1]
-       xormask = ord(sys.argv[2])
-                 text = sys.argv[3]
+xormask = ord(sys.argv[2])
+text = sys.argv[3]
 
-                        r = []
+r = []
 
-                            s = "0x%02x" % xormask
-                                for i in text:
-                                r.append( ord(i) ^ xormask )
-                                    xormask = ((xormask << 1) | (xormask >> 7)) & 0x0ff
+s = "0x%02x" % xormask
+for i in text:
+    r.append( ord(i) ^ xormask )
+    xormask = ((xormask << 1) | (xormask >> 7)) & 0x0ff
 
-                                        for i in r:
-                                            s+= (", 0x%02x" % i)
-                                                        print "static XoredStringA<%d> %s = {{" % (len(text)+1,name)
-                                                        print s
-                                                        print "}};"
+for i in r:
+    s+= (", 0x%02x" % i)
+print "static XoredStringA<%d> %s = {{" % (len(text)+1,name)
+print s
+print "}};"
